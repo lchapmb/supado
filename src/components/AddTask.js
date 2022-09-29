602b51e1ccc0f74b03b0d58c907b72c7b5abb947
lewis.chapman-barker <lewis.chapman-barker@timpson.com>
2022-09-29 11:12:22 +0100

    adds handleSubmit and controls to addtask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,11 +1,48 @@
-import { Button, HStack, Input } from '@chakra-ui/react';
+import { Button, HStack, Input, useToast } from '@chakra-ui/react';
+import { useState } from 'react';
+import supabase from '../supabase';
 
 export default function AddTask() {
+  const [text, setText] = useState('');
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const { data, error } = await supabase.from('todos').insert([{ text }]);
+
+    setLoading(false);
+
+    toast({
+      title: error || 'Todo added!',
+      position: 'top',
+      status: error ? 'error' : 'success',
+      duration: 2000,
+      isClosable: true,
+    });
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <HStack my="4" h="45">
-        <Input h="100%" variant="filled" placeholder="Do the washing"></Input>
-        <Button colorScheme="blue" px="10" h="100%" type="submit">
+        <Input
+          h="100%"
+          variant="filled"
+          placeholder="Do the washing"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <Button
+          colorScheme="blue"
+          px="10"
+          h="100%"
+          type="submit"
+          isLoading={loading}
+          loadingText="Adding"
+        >
           Add
         </Button>
       </HStack>
